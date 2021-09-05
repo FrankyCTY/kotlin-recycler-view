68eed95750a0b8f526900e0d2afd76c4d8fc0067
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-09-06 01:51:31 +1200

    init

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just an Affirmation object.
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
+    }
+
+    /**
+     * Create new views (invoked by the layout manager)
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view, layout inflater helps connect with view
+        // inflater helps create a view hierarchy for the list item LAYOUT resource
+        // so that we can use the view instance to locate widget required to bind data to
+        // them in onBindViewHolder.
+
+        // NOTE: LayoutInflater is used to create new View from XML, different from findViewById()
+        // And in activity.kt we can use getLayoutInflater().inflate(R.layout.my_layout, mainLayout, false);
+        // because it comes with Activity class
+        val inflatedItemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        // parent is the recycler view
+
+        return ItemViewHolder(inflatedItemView)
+    }
+
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
+    }
+
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
+    override fun getItemCount() = dataset.size
+}
